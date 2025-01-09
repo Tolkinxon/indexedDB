19ee17af370ae91a547808584653043c44693c9d
Soliyev Tolkinxon <krosstony639@gmail.com>
2025-01-09 23:06:24 +0500

    Add file preaparing

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-window.indexedDB
+const elForm = document.querySelector('.js-form');
+const elInput = document.querySelector('.js-input');
+const elList = document.querySelector('.js-list');
+
+elForm.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+
+    // console.dir(elInput.files[0]);
+    console.log(window);
+    
+    
+})
