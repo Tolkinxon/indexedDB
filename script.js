f0ffab07b9b781cceb1680e4799280ad1b400833
Soliyev Tolkinxon <krosstony639@gmail.com>
2025-01-09 23:37:16 +0500

    Add some code for creatte indexedDB

--- script.js
+++ script.js
@@ -2,11 +2,38 @@ const elForm = document.querySelector('.js-form');
 const elInput = document.querySelector('.js-input');
 const elList = document.querySelector('.js-list');
 
+const nameIndexedDB = 'indexedDB';
+let request = window.indexedDB.open(nameIndexedDB, 1); 
+let db;
+
+request.onsuccess = (evt) => {
+    db = evt.target.result;
+}
+
+request.onerror = (err) => {
+    console.log(err);
+    
+}
+
+request.onupgradeneeded = (evt) => {
+    db = evt.target.result;
+}
+
+
+
+
+
 elForm.addEventListener('submit', (evt) => {
     evt.preventDefault();
 
     // console.dir(elInput.files[0]);
     console.log(window);
+
+    const reader = new FileReader();
+
+    reader.readAsDataURL(elInput.files[0])
+
+
     
     
 })
